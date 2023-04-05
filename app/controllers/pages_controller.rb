1e7cc9b808f94cea1cdd579ecf76915df867fb73
fanny jousset <fanny.jousset@gmail.com>
2023-04-05 13:11:05 +1000

    reorganisation of files

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,19 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about
+  end
+
+  def education
+  end
+
+  def skills
+  end
+
+  def portfolio
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :articles
   root to: "pages#home"
+  get '/about', to: 'pages#about'
+  get '/education', to: 'pages#education'
+  get '/skills', to: 'pages#skills'
+  get '/portfolio', to: 'pages#portfolio'
+  get '/contact', to: 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
